3b5e31bfc2d37807f1be9488a4eda878e21b7b0e
Zoki91 <zmitrevski2@gmail.com>
2022-09-21 21:32:16 +1000

    models created

---
+++ models/Thought.js
@@ -0,0 +1,82 @@
+// Importing Mongoose and Moment JS
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment')
+
+// Creating the Reaction Schema
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+  },
+
+  username: {
+    type: String,
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => moveMessagePortToContext(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+  },
+
+  toJSON: {
+    virtuals: true,
+    getters:true
+  },
+  id: false,
+  }
+);
+
+// Creating the Thought Schema
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => moveMessagePortToContext(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+  },
+
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters:true
+  },
+  id: false,
+}
+);
+
+
+// Create a Virtual Property `reactionCount` that gets and sets reaction count
+// Homework 22 Stu Virtuals
+
+thoughtSchema.
+virtual('reactionCount')
+.get(function () {
+  return this.reactions.length;
+});
+
+// Create's the Thought model using the thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+// Exports the Thought model
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,62 @@
+// Importing Mongoose
+const { Schema, model } = require("mongoose");
+
+// Homework 23 Subdoc Population
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // Validating Email Address with Mongoose - https://thewebdev.info/2022/03/16/how-to-validate-email-syntax-with-mongoose/
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtual: true,
+    },
+    id: false,
+  }
+);
+
+
+// Create a Virtual Property `friendCount` that gets and sets reaction count
+// Homework 22 Stu Virtuals
+
+userSchema.
+virtual('friendCount')
+.get(function () {
+  return this.friends.length;
+});
+
+// Create's the User model using the userSchema
+const User = model('User', userSchema);
+
+// Exports the User model
+module.exports = User;
